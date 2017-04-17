d6b3c1ead763aae832d03262ddbbcf3cecfeed74
Thomas Foster <thomasfoster90>
2017-04-17 10:04:36 -0500

    fixed projects

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+$('.js-wp-1').waypoint(function(direction) {
+    $('.js-wp-1').addClass('animated fadeInDown');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-2').waypoint(function(direction) {
+    $('.js-wp-2').addClass('animated fadeInLeft');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-3').waypoint(function(direction) {
+    $('.js-wp-3').addClass('animated fadeInRight');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-4').waypoint(function(direction) {
+    $('.js-wp-4').addClass('animated fadeInUp');
+}, {
+    offset: '50%'
+});
+$('.js-wp-5').waypoint(function(direction) {
+    $('.js-wp-5').addClass('animated zoomInUp');
+}, {
+    offset: '50%'
+});
